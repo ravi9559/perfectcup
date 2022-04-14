f26733fc9f4b275a9bda316337985d380d04ffb9
Ravi Chaudhary <ravi.eng2012@gmail.com>
2022-04-14 13:22:41 +0530

    Peredctcup blog

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <p>Copyright &copy; The Perfect Cup <?php echo date("Y");?></p>
+                </div>
+            </div>
+        </div>
+    </footer>
