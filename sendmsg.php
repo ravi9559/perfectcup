c172bd869215370cc1c4c1474d84476a3111bdb3
Ravi Chaudhary <ravi.eng2012@gmail.com>
2022-04-18 16:30:26 +0530

    remove credentials

--- sendmsg.php
+++ sendmsg.php
@@ -50,7 +50,7 @@ if (strlen($fname) > 50) {
     $mail->isSMTP();                                      // Set mailer to use SMTP
     $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'ravi.eng2012@gmail.com';                 // SMTP username
+    $mail->Username = '';                 // SMTP username
     $mail->Password = '';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
